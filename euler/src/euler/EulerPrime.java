3a0b61ab67a7da78979a89f6feac974ab43761ab
vitooh <49574943+vitooh@users.noreply.github.com>
2019-10-24 09:52:47 +0200

    problem 03

---
+++ euler/src/euler/EulerPrime.java
@@ -0,0 +1,39 @@
+package euler;
+
+public class EulerPrime {
+	
+	public static boolean isPrime(final long i) {
+		if (i == 2 || i == 3 || i == 5) {
+			return true;
+		}
+		if (i % 2 == 0) {
+			return false;
+		}
+		for (long j = 3; j < i / j; j += 2) {
+			if (i % j == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(final String[] args) {
+		if (isPrime(11L)) {
+			System.out.println("isPrime seems to be working");
+		}
+		if (nextPrime(11) == 13) {
+			System.out.println("nextPrime seems to be working");
+		}
+	}
+	
+	static long nextPrime(long j) {
+		while (true) {
+			j++;
+			if (isPrime(j)) {
+				return j;
+			}
+			
+		}
+	}
+	
+}
